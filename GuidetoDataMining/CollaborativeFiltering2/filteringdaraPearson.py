c80ef7d2deefe1b5ebb398bf3840adf03cd4a340
Gladysgong <531404376@qq.com>
2017-10-18 10:02:46 +0800

    协同过滤--推荐--欧式距离--皮尔逊系数

--- GuidetoDataMining/CollaborativeFiltering2/filteringdaraPearson.py
+++ GuidetoDataMining/CollaborativeFiltering2/filteringdaraPearson.py
@@ -38,13 +38,56 @@ def manhattan(rating1, rating2):
         return -1  # Indicates no ratings in common
 
 
+# 明氏距离
+def minkowski(rating1, rating2, r):
+    """Computes the minkowski distance. Both rating1 and rating2 are dictionaries
+               of the form {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}"""
+    distance = 0
+    commonRatings = False
+    for key in rating1:
+        if key in rating2:
+            distance += pow(abs(rating1[key] - rating2[key]), r)
+            commonRatings = True
+    if commonRatings:
+        return pow(distance, 1 / r)
+    else:
+        return -1  # Indicates no ratings in common
+
+
+# pearson相关系数
+def pearson(rating1, rating2):
+    sum_xy = 0
+    sum_x = 0
+    sum_y = 0
+    sum_x2 = 0
+    sum_y2 = 0
+    n = 0
+    for key in rating1:
+        if key in rating2:
+            n += 1
+            x = rating1[key]
+            y = rating2[key]
+            sum_xy += x * y
+            sum_x += x
+            sum_y += y
+            sum_x2 += pow(x, 2)
+            sum_y2 += pow(y, 2)
+    # now compute denominator
+    denominator = sqrt(sum_x2 - pow(sum_x, 2) / n) * sqrt(sum_y2 - pow(sum_y, 2) / n)
+    if denominator == 0:
+        return 0
+    else:
+        return (sum_xy - (sum_x * sum_y) / n) / denominator
+
+
 # 寻找最近的用户
 def computeNearestNeighbor(username, users):
     """creates a sorted list of users based on their distance to username"""
     distances = []
     for user in users:
         if user != username:
-            distance = manhattan(users[user], users[username])
+            # distance = manhattan(users[user], users[username])
+            distance = minkowski(users[user], users[username],2)
             distances.append((distance, user))
     # sort based on distance -- closest first
     distances.sort()
